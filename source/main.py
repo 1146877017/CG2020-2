bcb746cdfce29981fea1716192ab458495811da9
forewing <jujianai@hotmail.com>
2020-03-27 14:36:06 +0800

    Update report

--- source/main.py
+++ source/main.py
@@ -1,25 +1,31 @@
-from model.Utils import *
-from model.Line import Line
-from model.Board import Board
+from model import *
 import numpy as np
 from PIL import Image
 
 if __name__ == "__main__":
-    board = Board(100, 200)
+    board: Board = Board(400, 400)
+
+    # board.addPrimitive(
+    #     1,
+    #     Polygon([(40, 20), (50, 50), (30, 100),
+    #              (10, 30), (20, 10)], Line.Algorithm.DDA)
+    # )
 
     board.addPrimitive(
         1,
-        Line(3, 2, 90, 170, Line.Algorithm.Bresenham)
+        Ellipse(150, 20, 20, 300)
     )
 
     board.setColor(ColorTable.RED)
 
     board.addPrimitive(
         2,
-        Line(50, 2, 20, 130, Line.Algorithm.Bresenham)
+        Ellipse(150, 20, 20, 300)
     )
 
-    board.translate(2, 50, 0)
+    # board.translate(2, 100, 100)
+    # board.scale(2, 0, 0, 0.5)
+    # board.rotate(2, 100, 200, 180)
 
     board.show()
     # board.save("test.bmp")
